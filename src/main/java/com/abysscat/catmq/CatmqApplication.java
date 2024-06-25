0af771d0612a4966b89e4284f8f75201a2513f2d
abysscat-yj <397967552@qq.com>
2024-06-26 00:43:00 +0800

    init

---
+++ src/main/java/com/abysscat/catmq/CatmqApplication.java
@@ -0,0 +1,13 @@
+package com.abysscat.catmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatmqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatmqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abysscat/catmq/CatmqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abysscat.catmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatmqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
